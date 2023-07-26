ef8e4a7b5d95706a99cd283597f86a9b4a0fb4f5
Jonatan <tarriba52@gmail.com>
2023-07-25 21:39:58 -0500

    practica metodos string

---
+++ 1-Introduccion/11-Metodos Strings/metodosStrings.js
@@ -0,0 +1,14 @@
+/*
+  📌 .length:  devuelve el numero  de caracteres de una cadena de texto 
+ */
+
+const texto = 'Hola soy Jonatan'
+console.log(texto.length)
+
+/*
+  📌 .indexOf()  devuelve el indice del primer caracter especificado
+      .lastIndexOf()   :  devuelve el indice del ultimo caracter especificado
+ */
+
+console.log(texto.indexOf('o'));   
+console.log(texto.lastIndexOf('o'));   
