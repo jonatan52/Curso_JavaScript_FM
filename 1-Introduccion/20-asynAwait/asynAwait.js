ddfe36b9e733d8615bb9fb91124d2bf22f82b0c0
Jonatan <tarriba52@gmail.com>
2023-08-03 17:53:01 -0500

    practica await

--- 1-Introduccion/20-asynAwait/asynAwait.js
+++ 1-Introduccion/20-asynAwait/asynAwait.js
@@ -2,20 +2,19 @@ const fetchPost = () => {
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
             const posts = ['Post1','Post2','Post3'];
-            const error = true;
+            const error = false;
 
             if(error){
                 reject('Huvo un error al intentar obtener los post');
             }else{
                 resolve(posts)
             }
-        }, 2000)
+        }, 2000);
     });
-}
+};
 
-fetchPost().then((posts)=>{
+const mostrarPost = async () => {
+    const posts = await fetchPost();
     console.log(posts);
-})
-.catch((error)=>{
-    console.log(error)
-})
+};
+mostrarPost();
