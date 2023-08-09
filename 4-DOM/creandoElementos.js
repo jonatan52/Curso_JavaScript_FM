e58c598ab0a7d78725cbeda5791bdaaf2675a33f
Jonatan <tarriba52@gmail.com>
2023-08-08 19:13:31 -0500

    practica

---
+++ 4-DOM/creandoElementos.js
@@ -0,0 +1,8 @@
+const agregarCaja = () =>{
+    /*
+      📌1. Creamos el elemento
+      createElement - Recibe como parametro una cadena de texto con la etiqueta 
+      que queremos crear  
+     */
+    const nuevaCaja = document.createElement('div')
+}
--- 4-DOM/modificandoElemento.js
+++ 4-DOM/modificandoElemento.js
@@ -12,3 +12,21 @@ primeraCaja.innerHTML = 'Hola';
 */
 
 primeraCaja.id = 'nuevo-id';
+
+/*
+  📌elemento.setaAttribute()
+  Nos permite agregar o establecer el valor de un atributo del elemento
+*/
+primeraCaja.setAttribute('class', 'caja activa');
+primeraCaja.setAttribute('data-id', '123-456-789');
+
+/*
+  📌elemento.style.property()
+  Nos permite cambiar estilos css de un elemento
+*/
+
+const contenedor2Caja1 = document.querySelector('#contenedor2 .caja');
+contenedor2Caja1.style.background = '#000';
+contenedor2Caja1.style.color = '#fff';
+contenedor2Caja1.style.textTransform = 'uppercase';
+
